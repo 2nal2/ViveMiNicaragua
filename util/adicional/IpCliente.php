ad0ffd8518c4af4831797027a50c46cc57c46f59
Crdzbird <luisalfonsocb83@gmail.com>
2016-06-13 14:48:34 -0600

    Mapa con ruta optima

---
+++ util/adicional/IpCliente.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'Objects/Sesion';
+require_once 'Objects/Usuario';
+
+/**
+ *
+ */
+class Ipcliente
+{
+
+    function __construct()
+    {
+
+    }
+
+    function get_cliente_ip() {
+        $ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+           $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
+    }
+
+}
+
+ ?>
