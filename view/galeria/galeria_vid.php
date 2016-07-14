b58895e2e01f93adcd91d9a8bb706538d2f9faaf
2nal2 <donaldov7@gmail.com>
2016-07-14 17:17:50 -0600

    cambios view video

--- view/galeria/galeria_vid.php
+++ view/galeria/galeria_vid.php
@@ -2,8 +2,17 @@
 <?php
   require_once '../../Model/VideoModel.php';
   require_once '../../Objects/Video.php';
+  require_once '../../Model/UsuarioModel.php';
+  require_once '../../Objects/Usuario.php';
 
   $videoModel = new VideoModel();
+  $usuarioModel = new UsuarioModel();
+
+  $CanEdit = false;
+  if(isset($_SESSION['id_user'])){
+    $u = $usuarioModel->getById($_SESSION['id_user']);
+    $CanEdit = $u->Rol=='Administrador' or $u->Rol=="Editor";
+  }
 
  ?>
 
@@ -18,6 +27,19 @@
 
 <section id="wrap_galerias">
     <h2>Galería de Videos</h2>
+    <?php if ($CanEdit): ?>
+
+      <div class="clear"></div>
+      <a class="clean" href="nuevo-video.php">
+        <button class="btn cuarto btn-new" type="button" name="button">
+          <span>
+            <i class="fa fa-plus-square" aria-hidden="true"></i>
+            Nuevo Video
+          </span>
+        </button>
+      </a>
+
+    <?php endif; ?>
     <?php foreach ($videoModel->getAll() as $video): ?>
       <?php
         $id = explode("/", $video->Url);
@@ -27,6 +49,15 @@
          <a class="fancybox-media" href="https://www.youtube.com/watch?v=<?php echo $video->Url ?>">
              <img class="thumb-vid" src="https://i.ytimg.com/vi/<?php echo $id ?>/mqdefault.jpg" alt="" />
          </a>
+         <!--inicio de codigo para editar  -->
+         <?php if ($CanEdit): ?>
+         <p>
+           <a class='clean' href="edit-video.php?id=<?php echo $video->IdVideo; ?>">
+             <button class="btn btn-edit" type="button" name="button"><span><i class="fa fa-pencil" aria-hidden="true"></i>  Editar</span></button>
+           </a>
+         </p>
+         <?php endif; ?>
+         <!--fin de codigo para editar  -->
        </div>
       <?php endforeach; ?>
 </section>
